0ca2155c6a92bcedb070821f54095850961ad0d2
santi <santiagobenfatto@outlook.com>
2025-05-21 23:46:33 -0300

    environment vars removed

--- src/dao/mysql/links.mysql.js
+++ src/dao/mysql/links.mysql.js
@@ -1,6 +1,6 @@
 export default class linksMySQL {
     constructor(connection) {
-        this.connection = connection;
+        this.connection = connection
     }
 
     getUserLinks = async (userId) => {
@@ -8,46 +8,46 @@ export default class linksMySQL {
             const result = await this.connection.execute({
                 sql: `SELECT * FROM links WHERE user_id = ?`,
                 args: [userId]
-            });
-            return result;
+            })
+            return result
         } catch (error) {
-            throw new DatabaseError(`Error al obtener enlaces del usuario con ID ${userId}: ${error.message}`);
+            throw new DatabaseError(`Error al obtener enlaces del usuario con ID ${userId}: ${error.message}`)
         }
-    };
+    }
 
     addLink = async (link) => {
         try {
             const result = await this.connection.execute({
                 sql: `INSERT INTO links(big_link, short_link, title, icon, alias) VALUES (?, ?, ?, ?, ?)`,
                 args: [link.big_link, link.short_link, link.title, link.icon, link.alias]
-            });
-            return result;
+            })
+            return result
         } catch (error) {
-            throw new DatabaseError(`Error al agregar el enlace '${link.title}': ${error.message}`);
+            throw new DatabaseError(`Error al agregar el enlace '${link.title}': ${error.message}`)
         }
-    };
+    }
 
     updateLink = async (linkId, updates) => {
         try {
             const result = await this.connection.execute({
                 sql: `UPDATE links SET title = ?, icon = ?, alias = ? WHERE id = ?`,
                 args: [updates.title, updates.icon, updates.alias, linkId]
-            });
-            return result;
+            })
+            return result
         } catch (error) {
-            throw new DatabaseError(`Error al actualizar el enlace con ID ${linkId}: ${error.message}`);
+            throw new DatabaseError(`Error al actualizar el enlace con ID ${linkId}: ${error.message}`)
         }
-    };
+    }
 
     removeLink = async (linkId) => {
         try {
             const result = await this.connection.execute({
                 sql: `DELETE FROM links WHERE id = ?`,
                 args: [linkId]
-            });
-            return result;
+            })
+            return result
         } catch (error) {
-            throw new CannotDelete(`Error al eliminar el enlace con ID ${linkId}: ${error.message}`);
+            throw new CannotDelete(`Error al eliminar el enlace con ID ${linkId}: ${error.message}`)
         }
-    };
+    }
 }
--- src/dao/mysql/users.mysql.js
+++ src/dao/mysql/users.mysql.js
@@ -1,56 +1,56 @@
 export default class UsersMySQL {
     constructor(connection) {
-        console.log('Working UsersDB with MySQL');
-        this.connection = connection;
+        console.log('Working UsersDB with MySQL')
+        this.connection = connection
     }
 
     getByEmailRegister = async (email) => {
         try {
             const result = await this.connection.execute({
-                sql: `SELECT first_name, last_name, nickname, email, hashed_pass FROM users WHERE email = ?;`,
+                sql: `SELECT first_name, last_name, nickname, email, hashed_pass FROM users WHERE email = ?`,
                 args: [email]
-            });
+            })
 
             if (!result || result.length === 0) {
-                throw new UserNotFound(`No se encontró un usuario con el email: ${email}`);
+                throw new UserNotFound(`No se encontró un usuario con el email: ${email}`)
             }
 
-            return result;
+            return result
         } catch (error) {
-            throw new DatabaseError(`Error al obtener usuario por email (${email}): ${error.message}`);
+            throw new DatabaseError(`Error al obtener usuario por email (${email}): ${error.message}`)
         }
-    };
+    }
 
     create = async (user) => {
         try {
             const result = await this.connection.execute({
-                sql: `INSERT INTO users (first_name, last_name, nickname, email, hashed_pass) VALUES (?,?,?,?,?);`,
+                sql: `INSERT INTO users (first_name, last_name, nickname, email, hashed_pass) VALUES (?,?,?,?,?)`,
                 args: [user.first_name, user.last_name, user.nickname, user.email, user.password]
-            });
+            })
 
-            return result;
+            return result
         } catch (error) {
             if (error.message.includes("Duplicate entry")) {
-                throw new UserAlreadyExists(`El usuario con email ${user.email} ya existe.`);
+                throw new UserAlreadyExists(`El usuario con email ${user.email} ya existe.`)
             }
-            throw new DatabaseError(`Error al crear usuario (${user.email}): ${error.message}`);
+            throw new DatabaseError(`Error al crear usuario (${user.email}): ${error.message}`)
         }
-    };
+    }
 
     updateById = async (userId, updates) => {
         try {
             const result = await this.connection.execute({
-                sql: `UPDATE users SET first_name = ?, last_name = ?, nickname = ?, email = ? WHERE user_id = ?;`,
+                sql: `UPDATE users SET first_name = ?, last_name = ?, nickname = ?, email = ? WHERE user_id = ?`,
                 args: [updates.first_name, updates.last_name, updates.nickname, updates.email, userId]
-            });
+            })
 
             if (result.affectedRows === 0) {
-                throw new UserNotFound(`No se encontró un usuario con ID: ${userId}`);
+                throw new UserNotFound(`No se encontró un usuario con ID: ${userId}`)
             }
 
-            return result;
+            return result
         } catch (error) {
-            throw new DatabaseError(`Error al actualizar usuario con ID (${userId}): ${error.message}`);
+            throw new DatabaseError(`Error al actualizar usuario con ID (${userId}): ${error.message}`)
         }
-    };
+    }
 }
