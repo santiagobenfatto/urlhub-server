2b099c6639438fe4960a08b0dda96aaa59b14adc
santi <santiagobenfatto@outlook.com>
2025-07-25 19:07:27 -0300

    test logs

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -29,7 +29,9 @@ export class UsersService {
     
     async register(userCredentials) {
         const checkUser = await this.usersRepository.checkUser(userCredentials.email_register)
-        //santi@gmail.com -> true / false
+        console.log('Incoming register request body:', req.body)
+        console.log('Cookies:', req.cookies)
+
         if(checkUser){
             throw new UserAlreadyExists('The email already exists')
         }
