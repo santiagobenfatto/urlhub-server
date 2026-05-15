c939c0a11e82bc4dae54d750d17645ce2a280376
santiagobenfatto <santiagobenfatto@outlook.com>
2026-05-15 12:03:00 -0300

    Fix validations

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -43,9 +43,8 @@ export class LinksController {
         try {
             const { title, big_link, icon, alias } = req.body
             const userId = req.user.id
-            logger.debug('THIS IS THE USER ID:', req.user)
 
-            if( !title || !big_link || !icon ) { 
+            if( !title || !big_link ) { 
                 return res.sendClientError(`Incomplete values`)
             }
 
